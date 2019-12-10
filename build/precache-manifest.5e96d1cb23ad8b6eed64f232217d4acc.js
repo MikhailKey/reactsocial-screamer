69a61c22cbc897bf908d1f62306747dcaad02585
MikhailKey <48489639+MikhailKey@users.noreply.github.com>
2019-12-10 15:23:42 +0400

    new commit

---
+++ build/precache-manifest.5e96d1cb23ad8b6eed64f232217d4acc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d05bfddbc5a382308a86fe6b2c9d44",
+    "url": "/reactsocial-screamer/index.html"
+  },
+  {
+    "revision": "139715d55423cc5863e5",
+    "url": "/reactsocial-screamer/static/css/main.f7bee724.chunk.css"
+  },
+  {
+    "revision": "1c09392d98eeafdfdf9b",
+    "url": "/reactsocial-screamer/static/js/2.62ca4664.chunk.js"
+  },
+  {
+    "revision": "139715d55423cc5863e5",
+    "url": "/reactsocial-screamer/static/js/main.dde81a51.chunk.js"
+  },
+  {
+    "revision": "07c474e51c546baad31b",
+    "url": "/reactsocial-screamer/static/js/runtime-main.cf0b837d.js"
+  },
+  {
+    "revision": "48f997ae7a801924cde1d5d849291c8e",
+    "url": "/reactsocial-screamer/static/media/login.48f997ae.png"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/reactsocial-screamer/static/media/profile-image.5a021ab9.png"
+  }
+]);
